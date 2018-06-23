f77c1c08cd6e3666dcfaedf5bed9579f3532993a
kwzeri <39018603+kwzeri@users.noreply.github.com>
2018-06-24 00:07:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-$(function(){
-$('h1').fadeIn(3600);
+  $(function(){
+  $('h1').fadeIn(3600);
 
+  // FAQのアコーディオン
+    $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+      
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+      
+    } else {
+      $answer.addClass('open'); 
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+      
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+    }
+  });
+  
 });
