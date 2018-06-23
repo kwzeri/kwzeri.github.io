0c2b7714127a0667b56908eba857049d10cd965a
kwzeri <39018603+kwzeri@users.noreply.github.com>
2018-06-24 00:52:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,10 @@
       // slideUpメソッドを用いて、$answerを隠してください
       $answer.fadeIn(500);
       
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $(this).find('span').transform: rotate(-90deg);
-      
     } else {
       $answer.addClass('open'); 
       // slideDownメソッドを用いて、$answerを表示してください
       $answer.fadeOut(1500);
-      
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $(this).find('span').transform: rotate(90deg);
     }
   });
   
