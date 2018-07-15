1b6d2bf59cc98273e994b5c7271141ee7ea5a898
kwzeri <39018603+kwzeri@users.noreply.github.com>
2018-07-16 02:40:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,30 @@
     }
   });
   
+    
+    
+    function slideSwitch() {
+   var $active = $('#photoshow img.active');
+
+   if ( $active.length == 0 ) $active = $('#photoshow img:last');
+
+   var $next =  $active.next().length ? $active.next()
+      : $('#photoshow img:first');
+
+   $active.addClass('last-active');
+
+   $next.css({opacity: 0.0})
+      .addClass('active')
+      .animate({opacity: 1.0}, 1000, function() {
+           $active.removeClass('active last-active');
+      });
+}
+
+$(function() {
+   setInterval( "slideSwitch()", 3000 );
+});
+    
+    
+    
+    
 });
