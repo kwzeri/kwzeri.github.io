7d58b39eb928a4268b1a4857d726aae1db529d90
kwzeri <39018603+kwzeri@users.noreply.github.com>
2018-07-19 23:11:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,34 @@
-  $(function(){
-	  
 
-//（１）ページの概念・初期ページを設定
-var page=0;
+$(function(){
 
-//（２）イメージの数を最後のページ数として変数化
-var lastPage =parseInt($("#slide img").length-1);
-
-//（３）最初に全部のイメージを一旦非表示にします
-     $("#slide img").css("display","none");
-
-//（４）初期ページを表示
-          $("#slide img").eq(page).css("display","block");
-
-//（５）ページ切換用、自作関数作成
-function changePage(){
-          $("#slide img").fadeOut(700);
-            $("#slide img").eq(page).fadeIn(700);
-};
-
-//（６）～秒間隔でイメージ切換の発火設定
-var Timer;
-function startTimer(){
-Timer =setInterval(function(){
-          if(page === lastPage){
-                         page = 0;
-                         changePage();
-               }else{
-                         page ++;
-                         changePage();
-          };
-     },5000);
-}
-//（７）～秒間隔でイメージ切換の停止設定
-function stopTimer(){
-clearInterval(Timer);
-}
-
-//（８）タイマースタート
-startTimer();
-
-/*オプションを足す場合はここへ記載*/
-  
-	
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-  $('h1').fadeIn(3600);
-
-    $('.faq-list-item').click(function() {
-    var $answer = $(this).find('.answer');
-    if($answer.hasClass('open')) {
-      $answer.removeClass('open');
-      // slideUpメソッドを用いて、$answerを隠してください
-      $answer.fadeIn(500);
-      
-    } else {
-      $answer.addClass('open'); 
-      // slideDownメソッドを用いて、$answerを表示してください
-      $answer.fadeOut(100);
+  // SNSボタン
+  $('.social-icon').hover(
+    function(){
+      $(this).children('span').animate({
+        'font-size':'30px'
+      }, 300);
+    },
+    function(){
+      $(this).children('span').animate({
+        'font-size':'24px'
+      }, 300);
     }
+  );
+  
+  // トップへ戻るボタン
+  $('#top-btn').click(function(){
+    $('html, body').animate({ 
+      'scrollTop': 0 
+    }, 500);
   });
   
-    
-    
-    
-    
-    
-
- 
-
-
-
-    
-    
-    
+  // 「header a」要素のclickイベントを作成してください。
+  $('header a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position 
+    }, 500);
+  });
+  
 });
